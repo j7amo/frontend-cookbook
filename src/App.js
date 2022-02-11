8daf91dcae4ba57cfeb33d4e885460b47f1f2b49
Roman Tarnakin <Roman_Tarnakin@epam.com>
2022-02-11 11:23:21 +0300

    feat: add Button component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+// именованный импорт (если соответствующий экспорт также был именованный) обязывает нас (1)использовать
+// фигурные скобки + явным образом (2)писать идентификатор компонента, который мы импортируем
+import { Button } from "./components/Button/Button";
+// import { Button1 } from "./components/Button/Button"; - так работать не будет -> идентификатор "Button1"
+// не экспортируется из "./components/Button/Button". При этом это не значит, что его там нет!
+// Если мы импортируем что-то, что было экспортировано по умолчанию (default export), то мы можем использовать
+// любое наименование при импорте!
+// import SomeSexyButton from "./components/Button/Button";
 
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					Learn React
-				</a>
+				<Button type="button">Don't click me!</Button>
+				{/*<SomeSexyButton>Click me!</SomeSexyButton>*/}
 			</header>
 		</div>
 	);
