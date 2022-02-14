fe50c9bb83f2fb87fea2d1fba73e1edcd92cd217
Roman Tarnakin <Roman_Tarnakin@epam.com>
2022-02-14 17:54:26 +0300

    feat: add AccordionItem component

--- src/components/Accordion/Accordion.jsx
+++ src/components/Accordion/Accordion.jsx
@@ -16,6 +16,10 @@ const someVerySexyAccordionData = [
 			'Tenetur ullam rerum ad iusto possimus sequi mollitia dolore ' +
 			'sunt quam praesentium. Tenetur ullam rerum ad iusto possimus sequi ' +
 			'mollitia dolore sunt quam praesentium.Tenetur ullam rerum ad iusto ' +
+			'possimus sequi mollitia dolore sunt quam praesentium.' +
+			'Tenetur ullam rerum ad iusto possimus sequi mollitia dolore ' +
+			'sunt quam praesentium. Tenetur ullam rerum ad iusto possimus sequi ' +
+			'mollitia dolore sunt quam praesentium.Tenetur ullam rerum ad iusto ' +
 			'possimus sequi mollitia dolore sunt quam praesentium.',
 	},
 	{
--- src/components/AccordionItem/AccordionItem.jsx
+++ src/components/AccordionItem/AccordionItem.jsx
@@ -6,20 +6,26 @@ import styles from './AccordionItem.module.scss';
 const AccordionItem = ({ question, answer }) => {
 	const [isOpen, setIsOpen] = useState(false);
 
-	const handleClick = () => setIsOpen(prevIsOpen => !prevIsOpen);
+	const handleClick = () =>
+		setIsOpen((prevIsOpen) => !prevIsOpen);
 
 	return (
-		<li className={styles.accordionItem}>
-			<button onClick={handleClick}>
+		<li>
+			<button
+				className={styles.accordionItemButton}
+				onClick={handleClick}
+			>
 				{question}
-				<span></span>
+				<span
+					className={styles.openClosedIndicator}
+				>
+					{isOpen ? '-' : '+'}
+				</span>
 			</button>
 			<div
 				className={classnames(
 					styles.answer,
-					isOpen
-						? styles.accordionItemIsOpen
-						: styles.accordionItemIsClosed,
+					!isOpen && styles.answerIsClosed,
 				)}
 			>
 				{answer}
@@ -28,6 +34,9 @@ const AccordionItem = ({ question, answer }) => {
 	);
 };
 
-AccordionItem.propTypes = {};
+AccordionItem.propTypes = {
+	question: PropTypes.string,
+	answer: PropTypes.string,
+};
 
 export default AccordionItem;
