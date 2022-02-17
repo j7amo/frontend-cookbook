40370f046b75278a06fb6b1e19b574f0b89b2759
Roman Tarnakin <Roman_Tarnakin@epam.com>
2022-02-17 19:36:47 +0300

    feat: finish adding HamburgerMenu component

--- src/components/HamburgerMenu/HamburgerMenu.jsx
+++ src/components/HamburgerMenu/HamburgerMenu.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 import styles from './HamburgerMenu.module.scss';
 
 // создадим компонент, который будет рисовать легендарное меню-гамбургер
@@ -7,24 +8,72 @@ import styles from './HamburgerMenu.module.scss';
 // будем принимать:
 // - isMenuOpen - показываем ли меню
 // - navItems - структуру данных, в которой будут лейблы для ссылок (элементов меню) и сами ссылки
-// - handleMenuClose - коллбэк, который мы будем вызывать при закрытии меню и изменять состояние родительского компонента
 // - toggleMenu - коллбэк, который меняет состояние родительского компонента
 const HamburgerMenu = ({
 	isMenuOpen,
 	navItems,
-	handleMenuClose,
 	toggleMenu,
 }) => {
+	const closeMenu = () => {
+		if (isMenuOpen) {
+			toggleMenu();
+		}
+	};
+	const toggleHamburgerMenu = (evt) => {
+		// ключевой момент: нужно остановить всплытие события "Click" с кнопки до <nav>
+		// иначе у нас будет 2 подряд вызова toggleMenu, которые по сути отменят друг друга
+		// и не позволят нам закрыть меню по нажатию на кнопку-крестик
+		evt.stopPropagation();
+		toggleMenu();
+	};
+	// используем useEffect для того, чтобы поменять стиль <body> после открытия и как следствие отрисовки модалки
+	useEffect(() => {
+		// ВАЖНО! Именно это ПОЗВОЛЯЕТ УБРАТЬ СКРОЛЛ БЭКГРАУНДА!!!
+		// в отличии от похожей реализации в компоненте Модального окна
+		// здесь нужно проверять флаг isMenuOpen, а также делать апдейт
+		// при изменении этого флага, иначе будет безусловное применение эффекта
+		if (isMenuOpen) {
+			document.body.style.overflow = 'hidden';
+		}
+		// при этом не забываем вернуть всё как было при закрытии и как размонтировании модалки
+		return () => {
+			document.body.style.overflow = 'unset';
+		};
+	}, [isMenuOpen]);
+
 	return (
-		<nav className={styles.mainNavigation}>
-			<button
-				type="button"
-				className={styles.hamburgerMenuButton}
-				onClick={toggleMenu}
+		<nav
+			className={classnames(
+				styles.mainNavigation,
+				isMenuOpen && styles.mainNavigationOpen,
+			)}
+			onClick={closeMenu}
+		>
+			{/*дополнительный контейнер, который будет абсолютно позиционирован*/}
+			<div className={styles.menuButtonContainer}>
+				<button
+					type="button"
+					className={classnames(
+						styles.hamburgerMenuButton,
+						isMenuOpen &&
+							styles.hamburgerMenuButtonOpened,
+					)}
+					onClick={toggleHamburgerMenu}
+				>
+					<span
+						className={styles.middleMenuBar}
+					/>
+				</button>
+			</div>
+			<ul
+				className={classnames(
+					styles.mainNavigationItems,
+					isMenuOpen &&
+						styles.mainNavigationItemsOpen,
+				)}
+				// прерываем всплытие события с меню
+				onClick={(evt) => evt.stopPropagation()}
 			>
-				<span className={styles.middleMenuBar} />
-			</button>
-			<ul className={styles.mainNavigationItems}>
 				{navItems.map(({ label, href }) => {
 					return (
 						<li
@@ -49,13 +98,14 @@ const HamburgerMenu = ({
 };
 
 HamburgerMenu.propTypes = {
+	isMenuOpen: PropTypes.bool,
 	navItems: PropTypes.arrayOf(
 		PropTypes.shape({
 			label: PropTypes.string,
 			href: PropTypes.string,
 		}),
 	),
-	handleMenuClose: PropTypes.func,
+	toggleMenu: PropTypes.func,
 };
 
 export default HamburgerMenu;
